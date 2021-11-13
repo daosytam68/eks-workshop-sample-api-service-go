374b9e0ffffa8f89f19b237f1d112a7c4278cc00
daosytam68 <52949768+daosytam68@users.noreply.github.com>
2021-11-13 18:21:35 +0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Welcome to CI/CD Pipeline DevAx Lab - TamDS (HDBank)"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
